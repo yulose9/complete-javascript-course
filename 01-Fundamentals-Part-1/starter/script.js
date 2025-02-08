f328d660e5c579214060cc9ca8b5e36bf61137f3
John Nazarene <jannazarene09@gmail.com>
2025-02-09 02:51:32 +0800

    Add .gitignore and update script.js with additional comparison examples

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -58,7 +58,7 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
- */
+
 
 const now = 2037;
 const ageJonas = now - 1991;
@@ -83,4 +83,25 @@ x--;
 console.log(x);
 
 // Comparison Operators
-console.log(ageJonas > ageSarah);
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+ */
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
