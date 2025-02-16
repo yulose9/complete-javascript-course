5f065792d0d7f42de377e4759f217bf31feb06c5
John Nazarene <jannazarene09@gmail.com>
2025-02-16 12:34:36 +0800

    Logical Operators Lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -165,7 +165,7 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1; // 10
 console.log(n);
- */
+
 
 // 5 falsy values: 0, '', undefined, null, NaN
 
@@ -189,3 +189,27 @@ if (height) {
 } else {
   console.log("Height is UNDEFINED");
 }
+
+
+const age = "18";
+if (age === 18) console.log("You just became an adult :D (strict)");
+
+if (age == 18) console.log("You just became an adult :D (loose)");
+
+const favourite = Number(prompt("What's your favorite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  //
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 27");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
+ */
