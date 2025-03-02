77c9277320ab228627ad7b6d628f9780d99f3ddb
John Nazarene <jannazarene09@gmail.com>
2025-03-02 18:38:19 +0800

    Logical Operators finale part 1 finsih

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -237,7 +237,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive...");
 }
- */
+
 
 const scoreDolphins = (96 + 108 + 89) / 3;
 const scoreKoalas = (88 + 91 + 110) / 3;
@@ -249,3 +249,91 @@ if (scoreDolphins > scoreKoalas) {
 } else {
   console.log("Both win the trophy");
 }
+
+
+const day = "sundaay";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+
+  default:
+    console.log("Not a valid day!");
+    break;
+}
+
+if (day == "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day == "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day == "wednesday" || day == "thursday") {
+  console.log("Write code examples");
+} else if (day == "friday") {
+  console.log("Record videos");
+} else if (day == "saturday" || day == "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
+
+
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+const me = "John";
+console.log(`I'm ${2025 - 2001} years old ${me}`);
+
+
+const age = 18;
+// age >= 18
+//   ? console.log("I like to drink wine 🍷")
+//   : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
+
+*/
+
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? 
+  `The bill was ${bill}, the tip was ${bill * 0.15}, and the total value ${bill + (bill * 0.15)}` : 
+  `The bill was ${bill}, the tip was ${bill * 0.20}, and the total value ${bill + (bill * 0.20)}`;
+
+console.log(tip);
+
