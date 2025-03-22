f69490416cdf1d6bc3c6f99a9489ae8279585059
John Nazarene <jannazarene09@gmail.com>
2025-03-23 02:38:13 +0800

    Add function declaration and expression examples for age calculation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -33,4 +33,20 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 console.log(num);
+
+
+// Function Declaration
+const age1 = calcAge(2001);
+function calcAge(birthYear) {
+  return 2025 - birthYear;
+}
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+  return 2025 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+
+console.log(`Age 1: ${age1}, Age 2: ${age2}`);
 */
