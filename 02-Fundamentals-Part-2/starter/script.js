26ba13a5fc663ae2cf402c461a6dc6a51df37d4b
John Nazarene <jannazarene09@gmail.com>
2025-03-30 17:51:05 +0800

    Intro to Objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -220,3 +220,19 @@ const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 */
+
+const nazareneArray = [
+  "John Nazarene",
+  "Dela Pisa",
+  2025 - 2001,
+  "Solution Architect",
+  ["Eunice", "Gail", "Glyzel"],
+];
+
+const nazarene = {
+  firstName: "John Nazarene",
+  lastName: "Dela Pisa",
+  age: 2025 - 2001,
+  job: "Solution Architect",
+  friends: ["Eunice", "Gail", "Glyzel"],
+};
