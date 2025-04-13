dc6032a9dce265ad73d1a91d337ac9e7b181f7ea
John Nazarene <jannazarene09@gmail.com>
2025-04-14 00:49:29 +0800

    setting up dev env

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,11 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const xasdfasdf = 23;
-if (x === 23) console.log(23);
+const x = '23';
 
 const calcAge = birthYear => 2025 - birthYear;
-
-console.log();
-
-// BUG FIXME
+console.log(calcAge(2001));
