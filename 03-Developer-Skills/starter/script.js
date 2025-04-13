b77c41a23e9a5edf8669eb9943b7e76a22d35984
John Nazarene <jannazarene09@gmail.com>
2025-04-14 00:38:51 +0800

    Setting up prettier

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xasdfasdf = 23;
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2025 - birthYear;
+
+console.log();
+
+// BUG FIXME
